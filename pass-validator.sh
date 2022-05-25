8a3f03568fdfb5793b468aa6ce9c98c1519187bd
omri ganini <omriganini6@gmail.com>
2022-05-25 19:54:11 +0300

    branch  Changes to be committed:

--- pass-validator.sh
+++ pass-validator.sh
@@ -1,6 +1,19 @@
 #! /bin/bash
 
 read -p "please enter password:" pass
-if [ ${#pass} -ge 10 ]; then echo "Great" ; exit
-else echo "Error"
+
+Branch_Regex="[a-z]"
+
+
+
+if [ ${#pass} -ge 10 ]
+then
+	echo "You have 10 Characters in your password " ; exit
+else echo "You don't have 10 Characters in your password"
+fi
+
+if [[ $pass =~ $Branch_Regex ]]
+then
+	echo "You have a lower case"
+else echo "You don't have lower case"
 fi
